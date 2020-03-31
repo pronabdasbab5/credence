29f806e43ca62bfcc47d46cc51aabeb46e3f68c9
Pronab Das <pronabdasbaba5@gmail.com>
2020-03-31 03:07:39 -0700

    Update

--- resources/views/web/include/header.blade.php
+++ resources/views/web/include/header.blade.php
@@ -30,156 +30,53 @@
               <div class="input-group-btn">
                 <button class="btn btn-default" type="submit"><i class="fa fa-search"></i> </button>
               </div>
-              <input type="text" class="form-control simple" placeholder="Search ..." name="srch-term" id="srch-term">
+              <input type="text" class="form-control simple" placeholder="Search ..." name="srch-term" id="search">
+              <div class="col-md-12" id="livesearch"> 
+                <!-- if no product fount on search  --> 
+                
+              </div>
             </div>
           </form>
         </div>
       </li>
-      <li><a href="index.html">Home</a>
+      <li><a href="{{ route('web.index') }}">Home</a></li>
+      @if(!empty($header_data['categories']) && (count($header_data['categories']) > 0))
+        @foreach($header_data['categories'] as $key => $item)  
+        @if(!empty($item['sub_categories']) && (count($item['sub_categories']) > 0))
+      <li><a>{{ $item['top_cate_name'] }}</a>
         <ul>
-          <li><a href="index.html">BigStart - Handbag</a></li>
-          <li><a href="Version2/index.html">BigStart - Fashion</a></li>
-          <li><a href="Version3/index.html">BigStart - Handbag V2</a></li>
+            @foreach($item['sub_categories'] as $keys => $items) 
+            @if (!empty($items->last_category) && (count($items->last_category) > 0))
+          <li><a><span>{{ $items->sub_cate_name }}</span></a>
+            <ul>
+                @foreach($items->last_category as $keyss => $itemss) 
+              <li> <a href="{{ route('web.product_list', ['slug' => $itemss->third_level_sub_category_name, 'top_category_id' => $item['top_category_id'], 'sub_category_id' => $items->id, 'last_category_id' => $itemss->id, 'sorted_by' => 0]) }}"> <span>{{ $itemss->third_level_sub_category_name }}</span> </a> </li>
+              @endforeach
+            </ul>
+          </li>
+          @else
+          <li> <a href="{{ route('web.product_list', ['slug' => $items->sub_cate_name, 'top_category_id' => $item['top_category_id'], 'sub_category_id' => $items->id, 'last_category_id' => 0, 'sorted_by' => 0]) }}"> <span>{{ $items->sub_cate_name }}</span> </a> </li>
+          @endif
+          @endforeach
         </ul>
       </li>
-      <li><a href="#">Pages</a>
-        <ul>
-          <li><a href="{{route('web.product.product-list')}}"><span>Shop Grid</span></a>
-            <ul>
-              <li> <a href="shop_grid_right.html"> <span>Shop Grid Right Sidebar</span> </a> </li>
-              <li> <a href="shop_grid_fullwidth.html"> <span>Shop Grid Fullwidth</span> </a> </li>
-            </ul>
-          </li>
-          <li> <a href="shop_list.html"> <span>Shop List</span> </a>
-            <ul>
-              <li> <a href="shop_list_right.html"> <span>Shop List Right Sidebar</span> </a> </li>
-            </ul>
-          </li>
-          <li> <a href="single_product.html"> <span>Single Product</span> </a> </li>
-          <li> <a href="shopping_cart.html"> <span>Shopping Cart</span> </a> </li>
-          <li><a href="checkout.html"><span>Checkout</span></a> </li>
-          <li> <a href="wishlist.html"> <span>Wishlist</span> </a> </li>
-          <li><a href="compare.html"><span>Compare</span></a> </li>
-          <li><a href="quick_view.html"><span>Quick View</span></a> </li>
-          <li><a href="404error.html"><span>404 Error Page</span></a> </li>
-        </ul>
-      </li>
-      <li><a href="#">Clutches</a>
-        <ul>
-          <li> <a href="{{route('web.product.product-list')}}"><span>tassel saddle bag</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Toaster Crossbody</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Piper Bag</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Leather Bag</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Canvas Bag</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>bucket bag</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Travel Accessories</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Gym Bags</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Fashion Waist Packs</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Messenger Bags</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>saddle bag</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Travel Duffels</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Umbrellas</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Waist Packs</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Travel Gear</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>curved boxy sling</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Luggage</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Briefcases</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Bowling</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Bucket Bag</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>floral lattice bag</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Crossbody Bag</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Clutch Handbag</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Hobo Shoulder</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Saddle Bag</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>Bag Accessories</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Wallet Wristlet</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Solo Premium </span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Laptop Bags</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Belts</span></a> </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-      <li><a href="{{route('web.product.product-list')}}">Handbags</a>
-        <ul>
-          <li> <a href="{{route('web.product.product-list')}}"><span>Steve Madden</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Elegant </span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>sassy &amp; chic</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Women's Satchel</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Shoulder Bag</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>College Bags</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>fashion Backpack</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Rose Gold Saffiano</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Black Velvet</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Leather Handbags</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>Material Bag</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Sling Bag</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Diaper Bags</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Laptop Backpack</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Benetton Black </span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>Lino Perros</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Graphite Croco</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Swiss Military Red</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Hidesign</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Benetton</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>Aquatan</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Travel Blue</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Charles & Keith</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>Clutches</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>wristlets</span></a> </li>
-            </ul>
-          </li>
-          <li> <a href="{{route('web.product.product-list')}}"><span>Kenneth Cole</span></a>
-            <ul>
-              <li> <a href="{{route('web.product.product-list')}}"><span>wallets</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>shoulder bags</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>leather totes</span></a> </li>
-              <li> <a href="{{route('web.product.product-list')}}"><span>satchels</span></a> </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-      <li><a href="{{route('web.product.product-list')}}">Laptop bags</a> </li>
-      <li><a href="{{route('web.product.product-list')}}">Furniture</a> </li>
-      <li><a href="{{route('web.product.product-list')}}">Luggage</a> </li>
-      <li><a href="contact-us.html">Backpacks</a> </li>
+      @else
+      <li><a href="{{ route('web.product_list', ['slug' => $item['top_cate_name'], 'top_category_id' => $item['top_category_id'], 'sub_category_id' => 0, 'last_category_id' => 0, 'sorted_by' => 0]) }}">{{ $item['top_cate_name'] }}</a> </li>
+      @endif
+      @endforeach
+    @endif
     </ul>
     <div class="jtv-top-link-mob">
       <ul class="links">
-        <li><a title="My Account" href="account_page.html">My Account</a> </li>
-        <li><a title="Wishlist" href="wishlist.html">Wishlist</a> </li>
-        <li><a title="Checkout" href="checkout.html">Checkout</a> </li>
-        <li><a title="Blog" href="blog.html"><span>Blog</span></a> </li>
-        <li class="last"><a title="Login" href="account_page.html"><span>Login</span></a> </li>
+        <li><a title="Wishlist" href="{{ route('web.wish_list') }}">Wishlist</a> </li>
+        <li><a title="Checkout" href="{{ route('web.view_cart') }}">Cart</a> </li>
+        @auth('users')
+        <li><a title="My Account" href="{{ route('web.my_profile') }}">My Account</a> </li>
+        <li><a title="Checkout" href="{{ route('web.logout') }}">Logout</a> </li>
+        @else
+            <li class="last"><a title="Login" href="{{ route('web.login') }}"><span>Login</span></a> </li>
+            <li class="last"><a title="Register" href="{{ route('web.registration_page') }}"><span>Register</span></a> </li>
+        @endauth
       </ul>
     </div>
   </div>
